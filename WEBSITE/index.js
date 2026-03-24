15dbebdf01054255e6243ad0a9cb0c0eeb3c874e
Quinten9789697 <quintkali1@gmail.com>
2026-03-24 10:31:48 +0100

    Connected api to index.js

--- WEBSITE/index.js
+++ WEBSITE/index.js
@@ -1,30 +1,37 @@
-function addBubble(user, text) { //* param user must be either "user" or "chatter" 
-    const parentElement = document.getElementById("chat-window");
-    const div = document.createElement("div");
-    const p = document.createElement("p");
+const ws = new WebSocket(`ws://$192.168.100.103:8080/ws`);
 
-    p.textContent = text;
-    div.appendChild(p);
-    div.classList.add("bubble", user);
+ws.onmessage = (event) => {
+  addBubble("chatter", event.data);
+};
 
-    parentElement.appendChild(div);
+function addBubble(user, text) {
+  //* param user must be either "user" or "chatter"
+  const parentElement = document.getElementById("chat-window");
+  const div = document.createElement("div");
+  const p = document.createElement("p");
 
-    parentElement.scrollTo(0, parentElement.scrollHeight);
+  p.textContent = text;
+  div.appendChild(p);
+  div.classList.add("bubble", user);
+
+  parentElement.appendChild(div);
+
+  parentElement.scrollTo(0, parentElement.scrollHeight);
 }
-
 
 const input = document.getElementById("message-input");
 
-input.addEventListener("keypress", function (event) { //* Check for enter usage when focused on input
-    if (event.key === "Enter") {
-        sendMessage();
-    }
+input.addEventListener("keypress", function (event) {
+  //* Check for enter usage when focused on input
+  if (event.key === "Enter") {
+    sendMessage();
+  }
 });
 
 function sendMessage() {
-    if (input.value !== "") {
-        addBubble("user", input.value);
-        //! API Send logic here
-    }
-    input.value = "";
+  if (input.value !== "") {
+    addBubble("user", input.value);
+    ws.send(input.value);
+  }
+  input.value = "";
 }
